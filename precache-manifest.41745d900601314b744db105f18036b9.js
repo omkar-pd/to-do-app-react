33a2c68c8d01710adc6abbe15ffe1a8c8fa21c6b
omkar-pd <omkarpd109@gmail.com>
2022-09-02 17:36:57 +0530

    Updates

---
+++ precache-manifest.41745d900601314b744db105f18036b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c15ee9704336a75f48c",
+    "url": "/to-do-app-react/static/css/main.56a89ce0.chunk.css"
+  },
+  {
+    "revision": "7c15ee9704336a75f48c",
+    "url": "/to-do-app-react/static/js/main.7c15ee97.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/to-do-app-react/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "52b5c45e92fe1b2881a7",
+    "url": "/to-do-app-react/static/js/2.52b5c45e.chunk.js"
+  },
+  {
+    "revision": "13d44e9dc06b4ad6e221",
+    "url": "/to-do-app-react/static/js/runtime~main.13d44e9d.js"
+  },
+  {
+    "revision": "54a5276d30d0891c739e47aec40f3cfa",
+    "url": "/to-do-app-react/index.html"
+  }
+];
